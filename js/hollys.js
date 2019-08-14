dcf52120c852c1609312354bc1fe8834673ae558
ChangMyFristWebPage <dkskrn2@naver.com>
2019-08-14 16:32:09 +0900

    Update hollys.js

--- js/hollys.js
+++ js/hollys.js
@@ -339,7 +339,7 @@ $('.pwfind i').click(function(){
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(37.573378, 126.974237), // 지도의 중심좌표
-        level: 5 // 지도의 확대 레벨
+        level: 2 // 지도의 확대 레벨
     };  
 
     // 지도를 생성합니다    
@@ -355,10 +355,10 @@ $('.pwfind i').click(function(){
     function placesSearchCB (data, status, pagination) {
         if (status === kakao.maps.services.Status.OK) {
 
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+           // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
             // LatLngBounds 객체에 좌표를 추가합니다
-            var bounds = new kakao.maps.LatLngBounds();
-
+          
+ 
             for (var i=0; i<data.length; i++) {
                 displayMarker(data[i]);    
                 bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
