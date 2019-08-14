5cb2af7e13bba3294fc79f5aa84d5532ab3cd965
ChangMyFristWebPage <dkskrn2@naver.com>
2019-08-14 16:55:13 +0900

    Update hollys.js

--- js/hollys.js
+++ js/hollys.js
@@ -354,18 +354,25 @@ $('.pwfind i').click(function(){
     var ps = new kakao.maps.services.Places(); 
 
     // 키워드로 장소를 검색합니다
-    ps.keywordSearch('할리스', placesSearchCB); 
+    ps.keywordSearch('할리스커피 서울', placesSearchCB); 
 
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
     function placesSearchCB (data, status, pagination) {
-        if (status === kakao.maps.services.Status.OK) {
-
-           // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-            // LatLngBounds 객체에 좌표를 추가합니다
-           
-        
-        } 
-    }
+      if (status === kakao.maps.services.Status.OK) {
+  
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+          // LatLngBounds 객체에 좌표를 추가합니다
+          var bounds = new kakao.maps.LatLngBounds();
+  
+          for (var i=0; i<data.length; i++) {
+              displayMarker(data[i]);    
+              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+          }       
+  
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+          map.setBounds(bounds);
+      } 
+  }
 
     // 지도에 마커를 표시하는 함수입니다
     function displayMarker(place) {
