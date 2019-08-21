14f584e939473bb32eee6230acfa74324149fec3
ChangMyFristWebPage <dkskrn2@naver.com>
2019-08-21 17:03:52 +0900

    2

--- js/hollys.js
+++ js/hollys.js
@@ -242,9 +242,9 @@ $('.pwfind i').click(function(){
   });
   //지도 열기
   $('.find #find').click(function(){
-    $('.map_wrap').css('width','1240px').css('height','540px').css('opacity','1').show(function(){      
-      
-    })
+    $('.map_wrap').css('width','1240px').css('height','540px').css('opacity','1').show(function(){
+      //지도불러오기
+      mapOpen();})
     $('.find div.black').show()
   })
   $('.find .map_wrap .material-icons').click(function(){
@@ -256,28 +256,67 @@ $('.pwfind i').click(function(){
     $('.find div.black').hide()
   })
     // 할리스 라이브
-  $('.hollys-live>div>div').click(function(){
-    var src=$(this).find('img').attr('src')
-    var alt=$(this).find('img').attr('alt')
-    var p=$(this).find('p').text()
-    var strong=$(this).find('strong').text()
-    $('.hollys-live .popup').show()
-    $('.hollys-live .black').show()
-    $('.popup .poto img').attr('src',src)
-    $('.popup .poto img').attr('alt',alt)
-    $('.popup .text p').text(p)
-    $('.popup .text strong').text(strong)
-  })
-  $('.hollys-live .black').click(function(){
-    $('.popup').hide()
-    $(this).hide()
-  })
-
-  $('.hollys-live .popup button').click(function(){
-    $('.hollys-live .popup').hide()
-    $('.hollys-live .black').hide()
-  })  
- 
-
-
+    $('.hollys-live>div>div').click(function(){
+      var src=$(this).find('img').attr('src')
+      var alt=$(this).find('img').attr('alt')
+      var p=$(this).find('p').text()
+      var strong=$(this).find('strong').text()
+      $('.hollys-live .popup').show()
+      $('.hollys-live .black').show()
+      $('.popup .poto img').attr('src',src)
+      $('.popup .poto img').attr('alt',alt)
+      $('.popup .text p').text(p)
+      $('.popup .text strong').text(strong)
+    })
+    $('.hollys-live .black').click(function(){
+      $('.popup').hide()
+      $(this).hide()
+    })
+    $('.hollys-live .popup button').click(function(){
+      $('.hollys-live .popup').hide()
+      $('.hollys-live .black').hide()
+    })  
+  
+  function mapOpen(){
+    var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(37.573378, 126.974237), // 지도의 중심좌표
+        level: 2 // 지도의 확대 레벨
+    };  
+    // 지도를 생성합니다    
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
+    // 장소 검색 객체를 생성합니다
+    var ps = new kakao.maps.services.Places(); 
+    // 키워드로 장소를 검색합니다
+    ps.keywordSearch('할리스커피 서울', placesSearchCB); 
+    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+    function placesSearchCB (data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {  
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+          // LatLngBounds 객체에 좌표를 추가합니다
+          var bounds = new kakao.maps.LatLngBounds();  
+          for (var i=0; i<data.length; i++) {
+              displayMarker(data[i]);    
+              bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+          }         
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+          map.setBounds(bounds);
+      } 
+  }
+    // 지도에 마커를 표시하는 함수입니다
+    function displayMarker(place) {        
+        // 마커를 생성하고 지도에 표시합니다
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place.y, place.x) 
+        });
+        // 마커에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, 'click', function() {
+            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+            infowindow.open(map, marker);
+        });
+    }
+  }
 })
